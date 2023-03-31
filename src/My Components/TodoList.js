549b45c9c31cbe0c7daaf7759efa6fc2986cdfbc
Divya Mishra <mishradivyagkp1999@gmailcom>
2023-03-31 23:04:08 +0530

    event done

--- src/My Components/TodoList.js
+++ src/My Components/TodoList.js
@@ -15,26 +15,25 @@ const TodoList = () => {
         setTasks([...tasks, myTodo]);
         console.log(tasks);
     };
-    const onChangingtitle = () =>{
-        console.log("title was changed");
+    // const onChanging = (e) =>{
+    //     settitle(e.target.value);
+    //     setdescription(e.target.value);
+    //     console.log("title was changed");
+    //     console.log("description was changed");
+    // }
 
-    }
-    const onChangingdesc =() =>{
-        console.log("description was changed");
-    }
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        settitle(e.target.value.title);
-        setdescription(e.target.value.description);
-        if (!title || !description) {
-            alert("Title or Description cannot be blank");
-        }
-        else {
-            addTask(title, description);
-        }
-        settitle('');
-        setdescription('');
+        // if (!title || !description) {
+        //     alert("Title or Description cannot be blank");
+        // }
+        // else {
+        //     addTask(title, description);
+        // }
+        addTask(title, description);
+        // settitle('');
+        // setdescription('');
     };
 
     return (
@@ -43,12 +42,18 @@ const TodoList = () => {
                 <div className="mb-3">
                     <label htmlFor="exampleInputEmail1" className="form-label">Title</label>
                     <input type="text" className="form-control" id="exampleInputEmail1" value ={title}
-                    onChange={onChangingtitle}  /> 
+                    onChange={(e) =>{
+                        const changedtitle = e.target.value;
+                        settitle(changedtitle);
+                    }}  /> 
                 </div>
                 <div className="mb-3">
                     <label htmlFor="exampleInputPassword1" className="form-label">Description</label>
                     <input type="text" className="form-control" id="exampleInputPassword1" value ={description}
-                       onChange={onChangingdesc} />  
+                       onChange={(e) =>{
+                        const changeddescription  = e.target.value;
+                        setdescription(changeddescription);
+                       }} />  
                 </div>
                 <button type="submit" className="btn btn-primary">Add Todo</button>
             </form>
