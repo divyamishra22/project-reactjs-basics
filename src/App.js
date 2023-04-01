bf532c19a000512b00eb0624072dbe1fcbc39832
Divya Mishra <mishradivyagkp1999@gmailcom>
2023-04-02 00:47:44 +0530

    about component added

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@
 import './App.css';
 //import React, { useState } from 'react';
 import TodoList from './My Components/TodoList';
+import Header from './My Components/Header';
+import About from './My Components/About';
 
 
 function App() {
  
   return (
        <>
+       <Header/>
       <TodoList />
+      <About/>
       </>      
    
   )
---
+++ src/My Components/About.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const About = () => {
+  return (
+    <div>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatibus distinctio dolorem at facilis laudantium non, 
+      sit fuga esse vitae, animi ullam rerum delectus architecto veniam autem quia illo omnis molestias!
+    </div>
+  )
+}
+
+export default About
