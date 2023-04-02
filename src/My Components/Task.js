65ea8bd2808814822524d8b81d9e8025ebf9d34e
Divya Mishra <mishradivyagkp1999@gmailcom>
2023-04-02 22:37:39 +0530

    update+fixed errors

--- src/My Components/Task.js
+++ src/My Components/Task.js
@@ -3,12 +3,12 @@
 const Task = ({task , onDelete}) => {
   return (
     <div className="task flex-row">
-      <div className="tasks">
+      <div className="tasks flex-col">
        <h3>{task.title}</h3>
        <p>{task.description}</p>
        </div>
        <div className='tasks-btn flex-row'>
-       <button className="btn btn-danger btn-sm" onClick={()=>{onDelete(task)}}>Delete</button> 
+       <button className="btn btn-danger btn-sm del-btn" onClick={()=>{onDelete(task)}}>Delete</button> 
        </div>
     </div>
   )
